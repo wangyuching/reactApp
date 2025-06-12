cf9976f33b393bb47379e9ec8850322a090b1724
王昱晴 <5b1g0023@stust.edu.tw>
2025-06-12 10:01:53 +0800

    Update MyDisplay.jsx

--- src/component/calculator/MyDisplay.jsx
+++ src/component/calculator/MyDisplay.jsx
@@ -4,6 +4,7 @@ function MyDisplay({ expression, result }) {
     <div className="my-calculator-display">
       <div className="expression">{expression || 0}</div>
       {/* 顯示運算式 */}
+      {/*如果 expression 有值，顯示 expression；如果沒有值，顯示 0。*/}
       <div className="result">{result !== "" ? result : ""}</div>
       {/* 顯示結果 */}
     </div>
