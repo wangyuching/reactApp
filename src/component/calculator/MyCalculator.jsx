a666e411847bc327ff299e6ec0bc4f5dbd75100c
王昱晴 <5b1g0023@stust.edu.tw>
2025-06-12 10:23:01 +0800

    Update MyCalculator.jsx

--- src/component/calculator/MyCalculator.jsx
+++ src/component/calculator/MyCalculator.jsx
@@ -1,4 +1,3 @@
-// MyCalculator 主元件，負責運算邏輯與畫面整合
 import React from "react";
 import MyDisplay from "./MyDisplay";
 import MyKeypad from "./MyKeypad";
@@ -6,11 +5,11 @@ import MyErrorMessage from "./MyErrorMessage";
 import "./MyCalculator.css";
 
 function MyCalculator() {
-  const [expression, setExpression] = React.useState("");
-  const [result, setResult] = React.useState("");
-  const [error, setError] = React.useState("");
+  const [expression, setExpression] = React.useState(""); // 因無使用 import { useState } from "react"，需用 React.useState 來呼叫 useState。
+  const [result, setResult] = React.useState(""); // 參考 8
+  const [error, setError] = React.useState(""); // 參考 8
 
-  const handleButtonClick = (value) => {
+  const handleButtonClick = (value) => { // 宣告一個 handleButtonClick 的函式，用來處理按鈕被點擊的事件。 value : 函式的參數。當按鈕被點擊時，所傳入的值。
     setError("");
     if (value === "C") {
       setExpression("");
