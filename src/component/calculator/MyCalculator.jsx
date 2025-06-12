4137614211c26001f498f28c260a83fb9b1f3032
王昱晴 <5b1g0023@stust.edu.tw>
2025-06-12 10:38:37 +0800

    Update MyCalculator.jsx

--- src/component/calculator/MyCalculator.jsx
+++ src/component/calculator/MyCalculator.jsx
@@ -10,12 +10,13 @@ function MyCalculator() {
   const [error, setError] = React.useState(""); // 參考 8
 
   const handleButtonClick = (value) => { // 宣告一個 handleButtonClick 的函式，用來處理按鈕被點擊的事件。 value : 函式的參數。當按鈕被點擊時，所傳入的值。
-    setError("");
+    setError(""); //將 error 狀態設為空字串
     if (value === "C") {
       setExpression("");
       setResult("");
     } else if (value === "←") {
       setExpression((prev) => prev.slice(0, -1));
+      // prev 代表目前的 expression 字串。slice(0, -1) 回傳新陣列 : 第 0 個元素到倒數第 1 個元素（不包含最後一個），藉此刪掉最後一個字元。
     } else if (value === "=") {
       try {
         const safeExp = expression.replace(/×/g, '*').replace(/÷/g, '/');
